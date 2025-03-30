9933ee5e5a513da60b78d592b8789edba0a3c1ed
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2025-03-30 15:06:25 +0530

    tailwind fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Nevbar from './components/Nevbar'
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+    <div className='px-6 sm:px-12 md:px-16 lg:px-30 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
 
 
     <Nevbar/>
--- client/src/components/Nevbar.jsx
+++ client/src/components/Nevbar.jsx
@@ -1,33 +1,29 @@
-import React, { useState } from 'react'
-import {assets} from '../assets/assets'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { assets } from '../assets/assets';
+import { Link } from 'react-router-dom';
 
-export const Nevbar = () => {
-
-    const [user,setUser] = useState(null);//ignore
+export const Navbar = () => {
+  const [user, setUser] = useState(null); // ignore
 
   return (
-    <div className='flex items-center justify-between py-4'>
-        <Link to = '/'>
-        <img src ={assets.logo} alt=""  className='w-28 sm:w-32 lg:w-40' />
-        </Link>
+    <div className="flex items-center justify-between py-4">
+      <Link to="/">
+        <img src={assets.logo} alt="Logo" className="w-28 sm:w-32 lg:w-40" />
+      </Link>
 
-        <div>
-            {user ? <div></div>
-            :
-            <div> 
-                <button>Login</button>
-            </div>
-            }
-            
-            
-        </div>
-
-
+      <div>
+        {user ? (
+          <div></div>
+        ) : (
+          <div className="flex items-center gap-2 sm:gap-5">
+            <button className=" bg-4  ">
+              Login
+            </button>
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default Nevbar
+export default Navbar;
