5c3fb19316570628204866b65ba7357d69ca3847
Goutham-Gourabathuni <156352020+Goutham-Gourabathuni@users.noreply.github.com>
2025-04-02 19:56:39 +0530

    Update Home.jsx
    is this correct way to write a header in jsx?

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <header id="header-main">
+        <div id="g-rbc-header-container" className="Header-styles__HeaderContainer-sc-97d88437-0 coLyoe">
+          <span className="g-rbc-header-sentinel-node" />
+          <div className="Header-styles__FixedContainer-sc-97d88437-1 gMlCnC" />
+        </div>
+        {/* Assuming mainContent is rendered elsewhere or as a prop */}
+      </header>
     </div>
   )
 }
